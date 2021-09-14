a7b2afa5c226f2adb1671d3e026e1ae6c23013ae
Moises <mhdezcoronado@gmail.com>
2021-09-14 17:59:24 +0200

    Create Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
+import { BsFillMicFill } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
 
-const Nav = () => {
-  const links = [
-    {
-      id: 2,
-      path: '/',
-    },
-    {
-      id: 3,
-      path: '/city',
-    },
-  ];
-
-  return (
-    <nav className="navBar">
-      <ul className="nav-list">
-        {links.map(link => (
-          <li key={link.id} className="nav-links">
-            <NavLink to={link.path} activeClassName="active-link" exact />
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+const Nav = () => (
+  <nav className="navBar">
+    <div className="back">
+      <IoIosArrowBack className="back-arrow" />
+      <span className="date">{new Date().toJSON().slice(0, 4)}</span>
+    </div>
+    <span className="all-cases">Cases in Europe</span>
+    <div className="tools">
+      <BsFillMicFill />
+      <FiSettings className="setting" />
+    </div>
+  </nav>
+);
 
 export default Nav;

31c5eec29319ea7ea32369ab2ac78565f6270034
Moises <mhdezcoronado@gmail.com>
2021-09-14 10:18:56 +0200

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import City from './components/City';
+import Country from './components/Country';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Country />
+          </Route>
+          <Route exact path="/city">
+            <City />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }

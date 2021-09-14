1eada8dce5c46e91ee395562e94aca97fa5cc607
Moises <mhdezcoronado@gmail.com>
2021-09-14 18:00:45 +0200

    Add stylesheet to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configStore';
 import App from './App';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>

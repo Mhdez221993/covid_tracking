41bd521c003b582adcc6c853dfae9ddb0b4e2fc0
Moises <mhdezcoronado@gmail.com>
2021-09-15 13:23:59 +0200

    Add navLink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { IoIosArrowBack } from 'react-icons/io';
 import { BsFillMicFill } from 'react-icons/bs';
 import { FiSettings } from 'react-icons/fi';
@@ -6,7 +7,9 @@ import { FiSettings } from 'react-icons/fi';
 const Nav = () => (
   <nav className="navBar">
     <div className="back">
-      <IoIosArrowBack className="back-arrow" />
+      <NavLink to="/" exact>
+        <IoIosArrowBack className="back-arrow" />
+      </NavLink>
       <span className="date">{new Date().toJSON().slice(0, 4)}</span>
     </div>
     <span className="all-cases">Cases in Europe</span>

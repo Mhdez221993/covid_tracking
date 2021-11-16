157ce0b3fe21409d61a1a0c727e460e24b471b40
Moises <mhdezcoronado@gmail.com>
2021-11-16 16:41:51 +0100

    Create temporary component to pass test

---
+++ src/features/cities/Cities.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Cities = () => <div>Cities component</div>;
+
+export default Cities;
---
+++ src/features/countries/Countries.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Countries = () => <div>Countries component</div>;
+
+export default Countries;

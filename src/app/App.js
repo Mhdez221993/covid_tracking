709858146aec996413d1dbf9f9cb89fbba59d6b1
Moises <mhdezcoronado@gmail.com>
2021-11-16 16:41:15 +0100

    Optimize App component and add routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+import Cities from '../features/cities/Cities';
+import Countries from '../features/countries/Countries';
+import Nav from '../features/Nav/Nav';
 
-const App = () => <div>Welcome to the covind 19 trakin app</div>;
+const routes = [
+  {
+    path: '/',
+    name: 'Countries',
+    component: <Countries />,
+  },
+  {
+    path: '/country/:name',
+    name: 'Cities',
+    component: <Cities />,
+  },
+];
+
+const App = () => (
+  <Router>
+    <Nav />
+    <Routes>
+      {routes.map(({ path, component }) => (
+        <Route key={path} path={path} element={component} />
+      ))}
+    </Routes>
+  </Router>
+);
 
 export default App;

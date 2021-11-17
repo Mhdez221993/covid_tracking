f450571b8e0524d24976bb93b215061f0a1eae51
Moises <mhdezcoronado@gmail.com>
2021-11-17 04:27:26 +0100

    Optimize coutriesReducer

--- src/features/countries/countriesReducer.js
+++ src/features/countries/countriesReducer.js
@@ -1,11 +1,9 @@
-import * as API from '../../app/api';
+import { getCountries } from '../../app/api';
 
 const LOAD_COUNTRIES = 'countries/country/LOAD_COUNTRIES';
-const LOAD_COUNTRY = 'countries/country/LOAD_COUNTRY';
 const initialState = {
   totalConfirmed: 0,
   items: [],
-  selected: null,
 };
 
 export const loadCountries = payload => ({
@@ -13,41 +11,22 @@ export const loadCountries = payload => ({
   payload,
 });
 
-export const loadCountry = payload => ({
-  type: LOAD_COUNTRY,
-  payload,
-});
-
-export const fetchCountry = name => async dispatch => {
-  const data = await API.getCountry(name);
-
-  dispatch(loadCountry(data));
-};
-
 export const fetchCountries = name => async dispatch => {
-  const cities = await API.getCountries(name);
-  const data = Object.values(cities).reduce((accumulator, currentValue) => {
-    const { All: { country, confirmed } } = currentValue;
-
-    accumulator.items.push({ name: country, confirmed });
-    accumulator.totalConfirmed += confirmed;
-
-    return accumulator;
-  }, {
-    totalConfirmed: 0,
-    items: [],
-  });
-
-  data.items = data.items.sort((a, b) => b.confirmed - a.confirmed);
-  dispatch(loadCountries(data));
+  const cities = await getCountries(name);
+  if (cities) {
+    Object.values(cities).forEach(({ All: { country, confirmed } }) => {
+      dispatch(loadCountries({ country, confirmed }));
+    });
+  }
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_COUNTRIES:
-      return action.payload;
-    case LOAD_COUNTRY:
-      return { ...state, selected: action.payload };
+      return {
+        items: [...state.items, action.payload],
+        totalConfirmed: state.totalConfirmed + action.payload.confirmed,
+      };
     default:
       return state;
   }

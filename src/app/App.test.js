9b58ee400fe2058ba110e4ce172598e748361ab4
Moises <mhdezcoronado@gmail.com>
2021-11-17 11:17:07 +0100

    Test App component

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { createTestStore, testRender } from '../test-utils';
 import App from './App';
 
-test('renders Welcome to the covind 19 trakin app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to the covind 19 trakin app/i);
-  expect(linkElement).toBeInTheDocument();
+let store;
+describe('App Component with API call', () => {
+  beforeEach(() => {
+    store = createTestStore();
+  });
+  test('Your component with a full reducer flow', async () => {
+    // Create a redux store
+    const { findByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    await findByText('Spain');
+  });
+
+  test('App snapshot test', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('App component without API call', () => {
+  test('Shour render Spain not in the document', () => {
+    testRender(<App />);
+    const linkElement = screen.queryByText('Spain');
+    expect(linkElement).not.toBeInTheDocument();
+  });
 });

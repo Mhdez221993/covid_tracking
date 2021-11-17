084a892cdc2769c14dc37189289578d9980d652f
Moises <mhdezcoronado@gmail.com>
2021-11-17 04:26:46 +0100

    Create citiesReducer

---
+++ src/features/cities/citiesReducer.js
@@ -0,0 +1,31 @@
+import * as API from '../../app/api';
+
+const SET_COUNTRY = 'countries/country/SET_COUNTRY';
+const initialState = [];
+
+export const setCountry = payload => ({
+  type: SET_COUNTRY,
+  payload,
+});
+
+export const fetchCountry = name => async dispatch => {
+  const data = await API.getCountry(name);
+
+  if (data) {
+    const list = Object.entries(data).slice(1);
+    list.forEach(([name, { confirmed }]) => {
+      dispatch(setCountry({ name, confirmed }));
+    });
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_COUNTRY:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default reducer;

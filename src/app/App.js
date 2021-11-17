4deb19c65a6be473175b5451f8faecccbc7bcf85
Moises <mhdezcoronado@gmail.com>
2021-11-17 11:16:52 +0100

    Optimize App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Route, Routes
+  BrowserRouter as Router, Route, Routes,
 } from 'react-router-dom';
 import Continent from '../features/continent/Continent';
 import Country from '../features/country/Country';

eaf321b2c5d6784f9a7a08f3050f48bbe41a4252
Moises <mhdezcoronado@gmail.com>
2021-11-17 01:58:20 +0100

    Create Countries component

--- src/features/countries/Countries.js
+++ src/features/countries/Countries.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FiArrowRightCircle } from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './countries.css';
+import { fetchCountries } from './countriesReducer';
+import europe from './europe.png';
 
-const Countries = () => <div>Countries component</div>;
+const Countries = () => {
+  const dispatch = useDispatch();
+  const { items, totalConfirmed } = useSelector(state => ({
+    ...state.countries,
+  }));
+
+  useEffect(
+    () => {
+      if (!items.length) dispatch(fetchCountries('europe'));
+    },
+    [],
+  );
+
+  return (
+    <div className="wrapper">
+      <div className="euro-wrapper">
+        <div className="euro-map">
+          <img src={europe} alt="europe" />
+        </div>
+        <div className="europ-description">
+          <p className="p-euro title-country">EUROPE</p>
+          {totalConfirmed}
+        </div>
+      </div>
+      <ul className="country-wrapper">
+        {items.map(v => (
+          <li key={v.name} className="country-item">
+            <Link to={`/country/${v.name}`} exact>
+              <FiArrowRightCircle className="arrow-right" />
+            </Link>
+            <div>
+              <span className="title-country">{v.name}</span>
+              <br />
+              <span className="title-number">{v.confirmed}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Countries;
